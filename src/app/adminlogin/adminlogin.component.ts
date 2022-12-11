2bbe50ffcbbfbdbd503b36650edd3c80cd692d2e
AmalAlby <amalalbyn@gmail.com>
2022-12-11 19:14:49 +0530

    complaint app all pages created

---
+++ src/app/adminlogin/adminlogin.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrls: ['./adminlogin.component.css']
+})
+export class AdminloginComponent {
+
+  constructor(private route:Router){
+    localStorage.removeItem("userInfo");
+  }
+  username = ""
+  password = ""
+  
+  readValues = ()=>{
+    if(this.username=="admin"&&this.password=="12345"){
+      this.route.navigate(["/dashboard"])
+    }else{
+      alert("Invalid Credentials");
+    }
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { DashComponent } from './dash/dash.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { UserregComponent } from './userreg/userreg.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoutes:Routes = [
+  {
+    path:"",
+    component:AdminloginComponent
+  },
+  {
+    path:"userlogin",
+    component:UserloginComponent
+  },
+  {
+    path:"userReg",
+    component:UserregComponent
+  },
+  {
+    path:"dashboard",
+    component:DashComponent
+  },
+  {
+    path:"userProfile",
+    component:UserprofileComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent,
+    DashComponent,
+    UserloginComponent,
+    UserprofileComponent,
+    UserregComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
